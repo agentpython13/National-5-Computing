e47e1b977815de332f540c065e43228e67ea6df4
Bennet <ybennetbthomas@gmail.com>
2024-09-27 12:52:57 +0100

    SDD 308a

---
+++ Software Design and Development/Input Validation.py
@@ -0,0 +1,14 @@
+print("Welcome to PointBox")
+print("===================")
+print()
+print("Please select an option from the main menu: ")
+print("1. log in")
+print("2. sign up for new account")
+print() 
+opt = input("> ")
+while opt != 1 and opt != 2:
+    print("Error. Try again!")
+    opt = input("> ")
+
+
+
