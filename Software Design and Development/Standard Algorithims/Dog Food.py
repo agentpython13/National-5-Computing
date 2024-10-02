40d118ab1dd2fbfb6bd381758c9a55853728fd89
Bennet <ybennetbthomas@gmail.com>
2024-10-02 09:12:16 +0100

    standard algorithims (308c)

---
+++ Software Design and Development/Standard Algorithims/Dog Food.py
@@ -0,0 +1,5 @@
+array = [26, 24, 22, 21, 25, 28, 32]
+sum = 0
+for grams in array:
+    sum += grams
+print(grams)
---
+++ Software Design and Development/Standard Algorithims/Temperature Traversing.py
@@ -0,0 +1,6 @@
+array = [26, 24, 22, 21, 25, 28, 32]
+sum = 0
+for temp in array:
+    sum += temp
+avg = sum / 7 
+print(round(avg,2))
